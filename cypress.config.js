42d0e5e360ff0b2cd3e4593743be48b07ff5ce96
noemie-ta-ma <noemie-ta-ma@users.noreply.github.com>
2026-04-14 14:54:50 +0000

    deploy: 07a6b4722c3ffed90623e6b691ca42b531d3051c

--- cypress.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'cypress';
-
-export default defineConfig({
-  e2e: {  // e2e = end to end, test l'app en entier
-    baseUrl: 'http://localhost:3000', // url du serveur vite en local
-  },
-});
