c743b57bf1ea7b948e7c944000aa5d024ddb2131
ShiryuMu8 <ShiryuMu8@users.noreply.github.com>
2026-04-15 09:29:58 +0000

    deploy: 553242319771d4a589bf8aaa8b9f575f206c3ded

--- two-ships/src/keys.js
+++
@@ -1,8 +0,0 @@
-export var keys_create = () => {
-  const keys = {};
-
-  addEventListener('keydown', (event) => (keys[event.code] = true));
-  addEventListener('keyup', (event) => (keys[event.code] = false));
-
-  return keys;
-};
